715d0596de2474288e7a09886f5ba0b9c6edc2ca
muraoka <taigamuraoka1998@gmail.com>
2020-05-12 15:06:57 +0900

    Add typescript

--- nextjs-blog/pages/posts/[id].tsx
+++ nextjs-blog/pages/posts/[id].tsx
@@ -3,8 +3,9 @@ import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import { GetStaticPaths, GetStaticProps } from 'next'
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds()
   return {
     paths,
@@ -12,8 +13,8 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string)
   return {
     props: {
       postData
@@ -21,11 +22,19 @@ export async function getStaticProps({ params }) {
   }
 }
 
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
-        <titile>{postData.titile}</titile>
+        <title>{postData.title}</title>
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
